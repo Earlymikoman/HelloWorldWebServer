d499f5f01163da92b45a9a1b1f32360bbd57ef7d
Xander Boosinger <xboosinger@gmail.com>
2026-01-27 09:08:04 -0800

    broooo

---
+++ Program.cs
@@ -0,0 +1,37 @@
+using System.ComponentModel.Design.Serialization;
+using System.Net;
+using System.Net.Security;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Microsoft.VisualBasic;
+
+class Program
+{
+
+    private static async Task DefaultDelegate(HttpContext context)
+    {
+        await context.Response.WriteAsync("Homepage");
+    }
+    private static async Task HelloWorldDelegate(HttpContext context)
+    {
+        await context.Response.WriteAsync("Hello World!");
+    }
+
+    private static async Task GoodbyeWorldDelegate(HttpContext context)
+    {
+        await context.Response.WriteAsync("Goodbye World!");
+    }
+
+    static void Main(String[] args)
+    {
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+        WebApplication app = builder.Build();
+
+        app.MapGet("/", DefaultDelegate);
+        app.MapGet("/hello", HelloWorldDelegate);
+        app.MapGet("/goodbye", GoodbyeWorldDelegate);
+
+        app.Run();
+    }
+}
